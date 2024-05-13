34d5e7da9857338811114dd5528db059e469b5b1
nourchamas <153526452+nourchamas@users.noreply.github.com>
2024-05-14 00:12:53 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,26 +13,22 @@ export default function About() {
         <div className="rows">
           <ExpandableRow title="Fiabilité">
             <p>
-              Toutes les annonces sont vérifiées, les propriétaires sont
-              sélectionnés et les photos sont certifiées.
+              Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
             </p>
           </ExpandableRow>
           <ExpandableRow title="Respect">
             <p>
-              Toutes les annonces sont vérifiées, les propriétaires sont
-              sélectionnés et les photos sont certifiées.
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
             </p>
           </ExpandableRow>
           <ExpandableRow title="Service">
             <p>
-              Toutes les annonces sont vérifiées, les propriétaires sont
-              sélectionnés et les photos sont certifiées.
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
             </p>
           </ExpandableRow>
           <ExpandableRow title="Sécurité">
             <p>
-              Toutes les annonces sont vérifiées, les propriétaires sont
-              sélectionnés et les photos sont certifiées.
+              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
             </p>
           </ExpandableRow>
         </div>
